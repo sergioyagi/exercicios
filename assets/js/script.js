05434e7515c7d7895a9c9317825f7306c92aa871
sergioyagi <yagi_sergio@hotmail.com>
2025-03-28 15:30:32 -0300

    Primeiro commit - adiconando arquivos do projeto

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+console.log("Script carregado corretamente!");
+// Exibe uma mensagem no console do navegador indicando que o script foi carregado com sucesso
+
+function saudacao() {
+    // Define uma função chamada "saudacao"
+    alert("Faça os exercícios!");
+    // Exibe uma caixa de alerta com a mensagem "Olá, Seja bem-vindo aos exercícios!"
+};
+
+saudacao();
+// Chama a função "saudacao", executando o código dentro dela (no caso, o alerta)
